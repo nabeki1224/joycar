b05f85d6aa98b77d7ed5d87a477472ce32834d4e
nabeki1224 <watanabe.kyo19991224@gmail.com>
2025-04-16 18:39:23 +0900

    add function for create car_list

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,4 +1,27 @@
 class CarsController < ApplicationController
   def new
+    @car = Car.new
+  end
+
+  def index
+    @car = Car.new
+    @cars = Car.all
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.save
+    redirect_to cars_path
+  end
+
+  def destroy
+    car = Car.find(params[:id])
+    car.destroy
+    redirect_to cars_path
+  end
+
+  private
+  def car_params
+    params.require(:car).permit(:name, :year)
   end
 end
